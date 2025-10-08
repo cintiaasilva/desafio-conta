20d8a042395d1f65ed93ed239833518faf81c82a
Cintia Silva <cintiazl2015@gmail.com>
2025-10-08 12:02:14 -0300

    feat: Criação da classe entidade

---
+++ src/main/java/com/cintia/desafio/Contas/model/Account.java
@@ -0,0 +1,31 @@
+package com.cintia.desafio.Contas.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@RequiredArgsConstructor
+@Data
+@Table(name = "conta")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String name;
+    @Column(name = "valor_original", nullable = false)
+    private BigDecimal originalValue;
+    @Column(name = "data_vencimento", nullable = false)
+    private LocalDate dueDate;
+    @Column(name = "data_pagamento", nullable = false)
+    private LocalDate paymentDate;
+    @Column(name = "valor_ajustado", nullable = false)
+    private BigDecimal adjustedAmount;
+    @Column(name = "dias_de_atraso", nullable = false)
+    private Integer daysLate;
+}
