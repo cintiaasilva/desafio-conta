93640c2d3fc32a70fcaae689b90f8f1f25ea0657
Cintia Silva <cintiazl2015@gmail.com>
2025-10-09 10:16:44 -0300

    fix: Alteração das anotações de validações de campos e da anotação do lombok para os construtores da classe

--- src/main/java/com/cintia/desafio/Contas/dto/AccountRequestDTO.java
+++ src/main/java/com/cintia/desafio/Contas/dto/AccountRequestDTO.java
@@ -1,21 +1,25 @@
 package com.cintia.desafio.Contas.dto;
 
-import lombok.Data;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@RequiredArgsConstructor
+@AllArgsConstructor
 @Data
+@NoArgsConstructor
 public class AccountRequestDTO {
-    @NonNull
+    @NotNull(message = "O nome não pode ser nulo.")
+    @NotBlank(message = "O nome da conta é obrigatório")
     private String name;
-    @NonNull
+    @NotNull(message = "O valor da conta é obrigatório")
+    @Positive(message = "O valor da conta deve ser positivo.")
     private BigDecimal originalValue;
-    @NonNull
+    @NotNull(message = "A data de vencimento da conta é obrigatório")
     private LocalDate dueDate;
-    @NonNull
+    @NotNull(message = "A data de pagamento da conta é obrigatório")
     private LocalDate paymentDate;
 }
