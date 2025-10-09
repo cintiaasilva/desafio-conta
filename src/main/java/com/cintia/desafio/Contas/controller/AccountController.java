eaee5f2c2bc1b5f37e828c020cdc5eb54a4df086
Cintia Silva <cintiazl2015@gmail.com>
2025-10-09 14:24:21 -0300

    docs: Implementação da documentação Swagger

--- src/main/java/com/cintia/desafio/Contas/controller/AccountController.java
+++ src/main/java/com/cintia/desafio/Contas/controller/AccountController.java
@@ -4,6 +4,10 @@ import com.cintia.desafio.Contas.dto.AccountRequestDTO;
 import com.cintia.desafio.Contas.dto.AccountResponseDTO;
 import com.cintia.desafio.Contas.model.Account;
 import com.cintia.desafio.Contas.service.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,17 +15,18 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
 @RequestMapping("/api/conta")
+@Tag(name = "Contas a pagar", description = "Endpoints para criar e listar contas.")
 public class AccountController {
 
     private final AccountService accountService;
 
-    public AccountController(AccountService accountService) {
-        this.accountService = accountService;
-    }
-
+    @Operation(description = "Inclui uma conta a pagar")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Retorna que a conta foi cadastrada com sucesso"),
+        @ApiResponse(responseCode = "400", description = "Retorna qual campo ou campos estão com os dados incorretos")
+    })
     @PostMapping("/pagamento")
     public ResponseEntity<Account> savePaidAccount(@RequestBody @Valid AccountRequestDTO accountDTO){
         try {
@@ -32,6 +37,12 @@ public class AccountController {
         }
     }
 
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @Operation(description = "Lista todos os registros das contas pagas")
+    @ApiResponse(responseCode = "200", description = "Retorna lista de contas pagas, ou uma lista vazia")
     @GetMapping
     public ResponseEntity<List<AccountResponseDTO>> listRegisteredAccounts(){
         List<AccountResponseDTO> accountResponseDTOS = accountService.listRegisteredAccounts();
